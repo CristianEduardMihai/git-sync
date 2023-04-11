ba777bafa24b7f8f76ee788cec2eedd61f65f7b5
Mihai Cristian <59600013+CristianEduardMihai@users.noreply.github.com>
2023-04-11 19:22:48 +0300

    Added test code

---
+++ sync.py
@@ -0,0 +1,88 @@
+import os
+import platform
+import subprocess
+import sys
+import time
+from github import Github
+import select
+
+# Replace with your Github username and Github token
+EMAIL = "email@example.com"
+USERNAME = "user"
+TOKEN = "xxxx"
+# Replace with the name of the repo you want to sync
+REPO_NAME = "repo name"
+
+#command to run after the sync is complete
+run_cmd = "python main.py"
+
+# Check if Git is installed, install it if not
+if platform.system() == "Linux":
+    result = subprocess.run(['dpkg', '-s', 'git'], capture_output=True)
+    if result.returncode != 0:
+        print("Git is not installed. Installing...")
+        os.system('apt-get update')
+        os.system('apt-get install -y git')
+
+# Authenticate with the Github API using the token
+g = Github(TOKEN)
+
+# Get the authenticated user
+user = g.get_user()
+
+# Make sure config email and user are inside ""
+config_email = f"\"{EMAIL}\""
+config_username = f"\"{USERNAME}\""
+
+# Check if the repository exists locally
+if not os.path.exists(REPO_NAME):
+    # Create a local repository with the specified name
+    os.mkdir(REPO_NAME)
+    os.chdir(REPO_NAME)
+
+    # If the repository doesn't exist locally, clone it from Github
+    repo = user.get_repo(REPO_NAME)
+    clone_url = repo.clone_url.replace("https://", f"https://{USERNAME}:{TOKEN}@")
+    os.system(f"git clone {clone_url} .")
+else:
+    # If the repository already exists, change to its directory
+    os.chdir(REPO_NAME)
+    
+    # Check if Git user configuration exists, set it if it doesn't exist
+    result = subprocess.run(['git', 'config', '--get', 'user.email'], capture_output=True)
+    if result.returncode != 0:
+        print("Git user email not set. Setting default Git user configuration...")
+        subprocess.run(['git', 'config', '--global', 'user.email', config_email])
+        subprocess.run(['git', 'config', '--global', 'user.name', config_username])
+
+    # If the repository exists locally, pull changes from Github
+    os.system("git fetch")
+    result = subprocess.run(['git', 'status', '--porcelain'], capture_output=True)
+    if result.stdout:
+        # If there are uncommitted changes, ask the user if they want to push changes or override with remote changes
+        print("Local changes detected.")
+        print("You have 10 seconds to decide if you want to push changes to Github.")
+        print("Press [y] to push changes, [n] to discard changes and re-pull or wait to skip any action.")
+        i, o, e = select.select([sys.stdin], [], [], 10)
+        if i:
+            push_changes = input("Do you want to push changes to Github? (y/n): ")
+            if push_changes.lower() == "y":
+                # If the user wants to push changes, commit and push the changes to Github
+                commit_message = input("Enter commit message: (push from git-sync)")
+                if commit_message=="":
+                    commit_message="push from git-sync"
+                os.system("git add .")
+                os.system(f'git commit -m "{commit_message}"')
+                os.system("git push")
+            elif push_changes.lower() == "n":
+                # If the user wants to override local changes with remote changes, discard local changes and pull from Github
+                os.system("git reset --hard")
+                os.system("git pull")
+        else:
+            print("Skipping github sync")
+    else:
+        # If there are no uncommitted changes, simply pull from Github
+        os.system("git pull")
+    
+# Start the main.py file
+os.system(run_cmd)
